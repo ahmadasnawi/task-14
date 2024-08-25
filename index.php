b92ad3949b11b32ac923335f39f578093cbdf41a
ahmadasnawi <ahmadaries899@gmail.com>
2024-08-25 08:06:26 +0700

    task 14

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Font Awesome Cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./src/css/main.css" />
+    <!-- JS -->
+    <script src="./src/js/app.js" defer></script>
+    <title>Disorganized</title>
+</head>
+
+<body>
+    <section>
+        <nav>
+            <div class="nav-left">
+                <div class="empty">
+                    <div></div>
+                </div>
+                <div class="empty-1"></div>
+            </div>
+            <div class="nav-right">
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Our Team</li>
+                    <li>About Us</li>
+                    <li>Log in</li>
+                </ul>
+            </div>
+            <div class="log-in">
+                <ul>
+                    <li>Log in</li>
+                </ul>
+            </div>
+        </nav>
+        <header>
+            <div class="container">
+                <div class="text">
+                    <h1>Experience the World with Virtual Reality</h1>
+                    <p>Drive into immersive virtual  world - Get  Now and experience the extraordinary. </p>
+                    <div class="bottom-line">
+                        <button>Get now</button>
+                        <i class="fa-solid fa-up-right-from-square"></i>
+                    </div>
+                </div>
+                <div class="image">
+                    <img src="./public/./assets/./image/gafas-virtuales.png" alt="">
+                    <img src="./public/./assets/./image/Untitled-1.png" alt="">
+                </div>
+            </div>
+        </header>
+    </section>
+    <div class="hero">
+        <div class="left-box">
+            <img src="./public/./assets/./image/metaverse-girl.9f00a869.png" alt="">
+            <i class="fa-solid fa-up-right-from-square"></i>
+        </div>
+        <div class="midle-box">
+            <div class="line-1">
+                <div class="kosong"></div>
+                <div class="nuhtoh">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="line-2">
+                <p>Indulgel in immersive 3D worlds. <br>Unlock limitless potential for your best <br>projects.</p>
+            </div>
+            <div class="line-3">
+                <button>Join now</button>
+                <i class="fa-solid fa-up-right-from-square"></i>   
+            </div>
+            <div class="line-4">
+                <div class="left">
+                    <div class="profile"></div>
+                    <div class="profile"></div>
+                    <div class="profile"></div>
+                </div>
+                <div class="number">
+                    <p>5100+</p>
+                    <p>New Users</p>
+                </div>
+            </div>
+        </div>
+        <div class="right-box">
+            <div class="top">
+                <i class="fa-solid fa-list"></i>
+            </div>
+            <div class="mid">
+                <h1>Features of VR</h1>
+            </div>
+            <div class="bottom">
+                <div class="left">
+                    <div>360View</div>
+                    <div>Virtual Gaming</div>
+                    <div>VR Cinemas</div>
+                </div>
+                <div class="right">
+                    <div>Spatial Audio</div>
+                    <div>Virtual Fitness</div>
+                    <div>Stress Relief</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
